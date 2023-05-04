8497a1d8c7e1f383bf3b130ada772e9edbce7aa7
JinHeeDo-prog <114121285+JinHeeDo-prog@users.noreply.github.com>
2023-05-04 16:40:31 +0900

    add Gender system

--- src/chapter_11/SIgnUp.jsx
+++ src/chapter_11/SIgnUp.jsx
@@ -2,12 +2,20 @@ import React, {useState} from "react";
 
 function SignUp(props){
   const [name,setName] = useState("");
+  const [gender, setGender] = useState("남자");
+
   const handleChangeName = (event) =>{
     setName(event.target.value);
   };
 
+  const handleChangeGender = (event) => { 
+    setGender(event.target.value);
+  }
+
   const handelSubmit = (event) => {
-    alert(`이름 : ${name}`);
+  
+    alert(`이름 : ${name}, 성별 : ${gender}`);
+  
     event.preventDefault();
   };
 
@@ -17,6 +25,14 @@ function SignUp(props){
         이름:
         <input type="text" value={name} onChange={handleChangeName}/>
 
+      </label>
+      <br/>
+      <label>
+        성별 : 
+        <select value={gender} onChange={handleChangeGender}>
+          <option value="남자">남자</option>
+          <option value="여자">여자</option>
+        </select>
       </label>
       <button type="submit">제출</button>
     </form>
